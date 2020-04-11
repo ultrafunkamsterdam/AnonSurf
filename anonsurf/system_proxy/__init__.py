0d6545825e95888ed9c3468e5aead5e5b30ddbb7
ultrafunkamsterdam <leon@ultrafunk.nl>
2020-04-11 15:53:06 +0200

    fix windows system proxy setting

--- anonsurf/system_proxy/__init__.py
+++ anonsurf/system_proxy/__init__.py
@@ -1,43 +1,45 @@
 import os
 import sys
-from ctypes import *
-from ctypes.wintypes import *
 
 from ..controller.controller import Tor
 
-LPWSTR = POINTER(WCHAR)
-HINTERNET = LPVOID
+if sys.platform == 'win32':
+    from ctypes import *
+    from ctypes.wintypes import *
 
-INTERNET_PER_CONN_PROXY_SERVER = 2
-INTERNET_OPTION_REFRESH = 37
-INTERNET_OPTION_SETTINGS_CHANGED = 39
-INTERNET_OPTION_PER_CONNECTION_OPTION = 75
-INTERNET_PER_CONN_PROXY_BYPASS = 3
-INTERNET_PER_CONN_FLAGS = 1
+    LPWSTR = POINTER(WCHAR)
+    HINTERNET = LPVOID
+
+    INTERNET_PER_CONN_PROXY_SERVER = 2
+    INTERNET_OPTION_REFRESH = 37
+    INTERNET_OPTION_SETTINGS_CHANGED = 39
+    INTERNET_OPTION_PER_CONNECTION_OPTION = 75
+    INTERNET_PER_CONN_PROXY_BYPASS = 3
+    INTERNET_PER_CONN_FLAGS = 1
 
 
-class INTERNET_PER_CONN_OPTION(Structure):
-    class Value(Union):
+    class INTERNET_PER_CONN_OPTION(Structure):
+        class Value(Union):
+            _fields_ = [
+                ("dwValue", DWORD),
+                ("pszValue", LPWSTR),
+                ("ftValue", FILETIME),
+            ]
+
         _fields_ = [
-            ("dwValue", DWORD),
-            ("pszValue", LPWSTR),
-            ("ftValue", FILETIME),
+            ("dwOption", DWORD),
+            ("Value", Value),
         ]
 
-    _fields_ = [
-        ("dwOption", DWORD),
-        ("Value", Value),
-    ]
 
-
-class INTERNET_PER_CONN_OPTION_LIST(Structure):
-    _fields_ = [
-        ("dwSize", DWORD),
-        ("pszConnection", LPWSTR),
-        ("dwOptionCount", DWORD),
-        ("dwOptionError", DWORD),
-        ("pOptions", POINTER(INTERNET_PER_CONN_OPTION)),
-    ]
+    class INTERNET_PER_CONN_OPTION_LIST(Structure):
+        _fields_ = [
+            ("dwSize", DWORD),
+            ("pszConnection", LPWSTR),
+            ("dwOptionCount", DWORD),
+            ("dwOptionError", DWORD),
+            ("pOptions", POINTER(INTERNET_PER_CONN_OPTION)),
+        ]
 
 
 def set_windows_system_proxy(ip, port, enabled=True):
@@ -79,14 +81,17 @@ def set_windows_system_proxy(ip, port, enabled=True):
 
 
 def set_system_proxy(tor: Tor, enabled: bool):
-    if enabled:
-        os.environ['HTTP_PROXY'] = f'127.0.0.1:{tor.http_tunnel_port}'
-        os.environ['HTTPS_PROXY'] = f'127.0.0.1:{tor.http_tunnel_port}'
-        os.environ['SOCKS_PROXY'] = f'127.0.0.1:{tor.port}'
-    else:
+    if not enabled:
         os.environ['HTTP_PROXY'] = ""
         os.environ['HTTPS_PROXY'] = ""
         os.environ['SOCKS_PROXY'] = ""
-    if sys.platform == 'win32':
-        return set_windows_system_proxy("", 0, False)
+        if sys.platform == 'win32':
+            return set_windows_system_proxy("", 0, False)
+    else:
+        os.environ['HTTP_PROXY'] = f'127.0.0.1:{tor.http_tunnel_port}'
+        os.environ['HTTPS_PROXY'] = f'127.0.0.1:{tor.http_tunnel_port}'
+        os.environ['SOCKS_PROXY'] = f'127.0.0.1:{tor.port}'
+
+        if sys.platform == 'win32':
+            return set_windows_system_proxy("127.0.0.1", tor.port, True)
     return True
